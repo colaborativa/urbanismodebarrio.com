dbd551050045f7500be88ea0459ce9dbd139e930
Javier Burón <javierburon@colaborativa.eu>
2012-08-23 13:02:26 +0200

    add query spam protection

--- js/main.js
+++ js/main.js
@@ -25,5 +25,16 @@ twttr.anywhere(function (T) {
 	T.hovercards();
 });
 $(document).ready(function(){
-    $("#imprimir").delay(1000).addClass('ensena');
+	$("#imprimir").delay(1000).addClass('ensena');
+	//Simple spam protection
+	$('#my-email').html(function(){
+		var e = "mas";
+		var a = "@";
+		var d = "urbanismodebarrio";
+		var c = ".com";
+		var h = 'mailto:' + e + a + d + c;
+		$(this).parent('a').attr('href', h);
+		return e + a + d + c;
+	});
 });
+
